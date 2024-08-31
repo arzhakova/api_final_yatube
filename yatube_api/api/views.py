94eeb3ddcb49cf733fa6987882f7f50e794e5b86
Anna Arzhakova <arzhakova.anna@yandex.ru>
2024-08-31 22:39:59 +0300

    version2_1

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -5,7 +5,7 @@ from rest_framework.pagination import LimitOffsetPagination
 from api.permissions import IsAuthorOrReadOnly
 from api.serializers import (CommentSerializer, FollowSerializer,
                              GroupSerializer, PostSerializer)
-from posts.models import Follow, Group, Post
+from posts.models import Group, Post
 
 
 class PostViewSet(viewsets.ModelViewSet):
